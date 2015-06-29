a10ec060ff451010e610ec0a26ecbed3038d95da
sepideh-a <s.alamdari@gmail.com>
2015-06-29 12:47:16 -0700

    done

--- Classes.h
+++ Classes.h
@@ -14,5 +14,28 @@ public:
 	Vector2D centerPoint;
 	int width;
 	int lenght;
+};
+
+
+class Bullets
+{
+public:
+	int ID;
+
+	Vector2D centerPoint;
+	
+};
+
+
+class Tanks
+{
+public:
+	int ID;
+
+	Vector2D centerPoint;
+	int width;
+	int length;
+
+	Vector2D velocity;
 
 };
